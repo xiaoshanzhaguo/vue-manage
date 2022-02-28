d96783339e23989a6fdb12ae4315b32f961cedbb
shanzhaguo <xiaoshanzhaguo@163.com>
2022-02-28 20:15:43 +0800

    complete element-ui结合脚手架使用之按需引入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button,Radio } from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Radio)
 
 new Vue({
   render: h => h(App),
